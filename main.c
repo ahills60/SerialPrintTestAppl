458582d3f4035b8747f2cff3afd0cbfd8f22ad5e
Andrew Hills <ahills60@gmail.com>
2015-09-14 11:31:29 +0100

    Adds basic main and start of serial comms
    - Basic main added with stop for watchdog timer.
    - Communication port initialised, although needs further configuration
    for clock selection.

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <msp430.h> 
+#include <string.h>
+#include "bsp.h"
+
+// Prototypes:
+void COM_Init(void);
+
+
+// Ancillary functions
+void COM_Init(void)
+{
+	P3SEL |= 0x30;					// P3.4,.5 = USCI_A0 TXD/RXD
+	UCA0CTL1 = UCSSEL_2;
+
+
+}
+
+/*
+ * main.c
+ */
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	
+	return 0;
+}
