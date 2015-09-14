94f38859cdac860397a2cb4ccb31111780a5f218
Andrew Hills <ahills60@gmail.com>
2015-09-14 11:41:59 +0100

    Changes tabs to spaces
    - Tabs have been converted to spaces within the main C file.

--- main.c
+++ main.c
@@ -9,8 +9,8 @@ void COM_Init(void);
 // Ancillary functions
 void COM_Init(void)
 {
-	P3SEL |= 0x30;					// P3.4,.5 = USCI_A0 TXD/RXD
-	UCA0CTL1 = UCSSEL_2;
+    P3SEL |= 0x30;                  // P3.4,.5 = USCI_A0 TXD/RXD
+    UCA0CTL1 = UCSSEL_2;
 
 
 }
@@ -19,7 +19,7 @@ void COM_Init(void)
  * main.c
  */
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	
-	return 0;
+    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
+    
+    return 0;
 }
